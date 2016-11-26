7249989091ef2b4f3b1994b239928d9ec8a18812
sara <sara@huhbub.com>
2016-11-26 08:50:04 +0000

    implements hit points

--- makers_academy/projects/challenges/Battle/spec/features/attack_spec.rb
+++ makers_academy/projects/challenges/Battle/spec/features/attack_spec.rb
@@ -1,7 +1,14 @@
 feature "Attacking" do
   scenario "attack Player 2" do
     sign_in_and_play
-    click_link "Attack"
+    click_button "Attack"
     expect(page).to have_content "Sara attacked Zelda"
+  end
+
+  scenario "reduce Player 2 HP by 10" do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).not_to have_content "Zelda: 60HP"
+    expect(page).to have_content "Zelda: 50HP"
   end
 end
--- makers_academy/projects/challenges/Battle/spec/player_spec.rb
+++ makers_academy/projects/challenges/Battle/spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 describe Player do
   subject(:sara) { Player.new('Sara') }
+  subject(:zelda) { Player.new('Zelda') }
 
   describe '#name' do
     it 'returns the name' do
       expect(sara.name).to eq 'Sara'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(sara.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(zelda).to receive(:receive_damage)
+      sara.attack(zelda)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { sara.receive_damage }.to change { sara.hit_points }.by(-10)
     end
   end
 end
