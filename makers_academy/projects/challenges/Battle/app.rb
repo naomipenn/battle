c95e0b2ba215e6c4fe6ca8653c7485593ae6e6f3
sara <sara@huhbub.com>
2016-11-26 12:16:09 +0000

    refactors controller to be skinnier

--- makers_academy/projects/challenges/Battle/app.rb
+++ makers_academy/projects/challenges/Battle/app.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require './lib/game'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -11,19 +13,18 @@ class Battle < Sinatra::Base
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
+    $game = Game.new($player_1, $player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
