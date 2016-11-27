9c9ce5dd61c22caaacdc9ec227afec23f90a6fe9
sara <sara@huhbub.com>
2016-11-27 22:32:00 +0000

    adds ability for Player 2's attack to reduce Player 1's HP

--- makers_academy/projects/challenges/Battle/spec/features/attack_spec.rb
+++ makers_academy/projects/challenges/Battle/spec/features/attack_spec.rb
@@ -22,4 +22,14 @@ feature "Attacking" do
     expect(page).to have_content "Zelda: 50HP"
   end
 
+  scenario "reduce Player 1 HP by 10" do
+    sign_in_and_play
+    click_button "Attack"
+    click_button "OK"
+    click_button "Attack"
+    click_button "OK"
+    expect(page).not_to have_content "Sara: 60HP"
+    expect(page).to have_content "Sara: 50HP"
+  end
+
 end
