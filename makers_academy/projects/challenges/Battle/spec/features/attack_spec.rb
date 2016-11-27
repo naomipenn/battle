82c0bd0c620049d898e13fd0a7e022543d15b706
sara <sara@huhbub.com>
2016-11-27 22:19:47 +0000

    adds ability for Player 2 to attack, with confirmation

--- makers_academy/projects/challenges/Battle/spec/features/attack_spec.rb
+++ makers_academy/projects/challenges/Battle/spec/features/attack_spec.rb
@@ -1,15 +1,25 @@
 feature "Attacking" do
+
   scenario "attack Player 2" do
     sign_in_and_play
     click_button "Attack"
     expect(page).to have_content "Sara attacked Zelda"
   end
 
+  scenario "be attacked by Player 2" do
+    sign_in_and_play
+    click_button "Attack"
+    click_button "OK"
+    click_button "Attack"
+    expect(page).to have_content "Zelda attacked Sara"
+  end
+
   scenario "reduce Player 2 HP by 10" do
     sign_in_and_play
     click_button "Attack"
-    click_link "OK"
+    click_button "OK"
     expect(page).not_to have_content "Zelda: 60HP"
     expect(page).to have_content "Zelda: 50HP"
   end
+
 end
--- makers_academy/projects/challenges/Battle/spec/features/switch_turns_spec.rb
+++ makers_academy/projects/challenges/Battle/spec/features/switch_turns_spec.rb
@@ -8,7 +8,7 @@ describe 'Switch turns' do
   scenario 'after player 1 attacks' do
     sign_in_and_play
     click_button 'Attack'
-    click_link 'OK'
+    click_button 'OK'
     expect(page).not_to have_content "Sara's turn"
     expect(page).to have_content "Zelda's turn"
     end
