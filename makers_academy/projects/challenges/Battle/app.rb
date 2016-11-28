269f9302b4a6d9f1e9129fca65e95e5a18f24e7f
sara <sara@huhbub.com>
2016-11-28 08:55:10 +0000

    kills global variable

--- makers_academy/projects/challenges/Battle/app.rb
+++ makers_academy/projects/challenges/Battle/app.rb
@@ -10,21 +10,24 @@ class Battle < Sinatra::Base
     erb :index
   end
 
+  before do
+  @game = Game.instance
+  end
+
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     erb :play
   end
 
   post '/attack' do
-    Attack.run($game.opponent_of($game.current_turn))
-    if $game.game_over?
+    Attack.run(@game.opponent_of(@game.current_turn))
+    if @game.game_over?
       redirect '/game-over'
     else
       redirect '/attack'
@@ -32,17 +35,15 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    @game = $game
     erb :attack
   end
 
   post '/switch-turns' do
-    $game.switch_turns
+    @game.switch_turns
     redirect('/play')
   end
 
   get '/game-over' do
-    @game = $game
     erb :game_over
   end
 
